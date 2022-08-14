a89dff1eaf4f4cde0212ba356ff193ada0e4e953
Aly <princealy.nueles@gmail.com>
2022-08-15 05:54:57 +0800

    Added Latest NEws section

---
+++ homee.js
@@ -0,0 +1,77 @@
+/* --- NAV CHANGE BG --- */
+
+var nav = document.querySelector('nav');
+   
+  window.addEventListener('scroll', function() {
+      if(window.pageYOffset > 100) {
+        nav.classList.add('bg-success', 'shadow');
+        document.getElementsByClassName('.dropdown-menu').style.backgroundColor = '#08331fce';
+      }else{
+        nav.classList.remove('bg-success','shadow')
+        document.getElementsByClassName('.dropdown-menu').style.backgroundColor = 'none';
+      }
+    })
+
+/* --- PLAY/PAUSE BTN --- */
+    let bgVid = document.querySelector(".backvid");
+    let pauseB = document.querySelector(".pauseBtn1");
+
+    // pauseB.addEventListener('click', function(pauseB){
+    //   pauseB.target.classList.toggle(".playBtn1")
+    // })
+
+    function playBtn() {
+        bgVid.play();
+    }
+
+    function pauseBtn() {
+        bgVid.pause();
+    }
+
+     function muteBtn() {
+      bgVid.muted=true;
+     }
+
+     function unmuteBtn() {
+      bgVid.muted=false;
+     }
+
+
+     const swiper = new Swiper('.swiper',  {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          hide: true,
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+        effect: 'fade',
+        },
+        spaceBetween: 30,
+        loop: true,
+
+        mousewheel: {
+          invert: false,
+        },
+
+        // autoHeight: true,
+      });
+      
+//   let swiper = new Swiper(".mySwiper", {
+//  effect: "cards",
+//  grabCursor: true,
+// });
