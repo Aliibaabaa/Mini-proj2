5aebbb4325b94717098edbbc859dd54850410fe4
Aly <princealy.nueles@gmail.com>
2022-08-23 12:38:50 +0800

    created sci game1 easy

---
+++ science_g1.js
@@ -0,0 +1,176 @@
+(function(){
+
+    function buildQuiz(){
+      const output = [];
+  
+      
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+          const answers = [];
+  
+          
+          for(letter in currentQuestion.answers){
+  
+            
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          
+          output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
+        }
+      );
+  
+      
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+     
+      let numCorrect = 0;
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+ 
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+      
+        if(userAnswer === currentQuestion.correctAnswer){
+
+          numCorrect++;    
+          answerContainers[questionNumber].style.color = 'green';
+        }
+        
+        else{
+          
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+  
+      
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    function showSlide(n) {
+      slides[currentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      currentSlide = n;
+      if(currentSlide === 0){
+        previousButton.style.display = 'none';
+      }
+      else{
+        previousButton.style.display = 'inline-block';
+      }
+      if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+      }
+      else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+      }
+    }
+  
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+   
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "What holds the planets in their orbits around the sun?",
+        answers: {
+          a: "gravity",
+          b: "speed",
+          c: "string"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Venus, Mercury, Earth, and Mars are what type of planet?",
+        answers: {
+          a: "Gas Giants",
+          b: "Terrestrial Planets",
+          c: "Ice Giants"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Jupiter, Saturn, Uranus, and Neptune are what type of planets?",
+        answers: {
+          a: "Gas Giants",
+          b: "Terrestrial Planets",
+          c: "Ice Giants",
+          d: "Exterior Planets"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Almost Earth’s twin in size and mass, this planet has very thick atmosphere.",
+        answers: {
+          a: "Venus",
+          b: "Mercury",
+          c: "Mars",
+          d: "Neptune"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "The planets are all equally distanced from each other.",
+        answers: {
+          a: "True",
+          b: "False",
+          
+        },
+        correctAnswer: "b"
+      },
+    ];
+  
+   
+    buildQuiz();
+
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+  
+    
+    showSlide(currentSlide);
+  
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+  })();
+
+
+
+
+ 
+  
+  
+  
+  
+  
